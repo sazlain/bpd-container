33fcf648fb3eabd83a8cd6817f4680f72aaccf1b
sazlain <44810664+sazlain@users.noreply.github.com>
2021-12-20 17:02:45 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 echo inicializando
 
+docker-compose stop
+
 rm -rf API-BPD
 git clone https://github.com/sazlain/API-BPD.git
+cd API-BPD
+git checkout main
+cd ..
 
 rm -rf best-parts-deals
 git clone https://github.com/estebanrp19/best-parts-deals.git
+cd best-parts-deals
+git checkout master
+cd ..
 
-docker-compose up -d
+docker-compose up -dp -d
